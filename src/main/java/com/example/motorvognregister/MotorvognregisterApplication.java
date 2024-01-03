931813ab4f20bb9a166c82d3bc37596fca5fb5c7
NymmkTheMink <141179443+NymmkTheMink@users.noreply.github.com>
2024-01-03 13:38:00 +0100

    initial commit

---
+++ src/main/java/com/example/motorvognregister/MotorvognregisterApplication.java
@@ -0,0 +1,13 @@
+package com.example.motorvognregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorvognregisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotorvognregisterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/motorvognregister/MotorvognregisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.motorvognregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorvognregisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
