945ca604f8e613fe9e356465a50c0a16a60af090
NymmkTheMink <141179443+NymmkTheMink@users.noreply.github.com>
2024-01-04 15:32:20 +0100

    imported frameworks

--- src/main/java/com/example/motorvognregister/MotorvognController.java
+++ src/main/java/com/example/motorvognregister/MotorvognController.java
@@ -1,4 +1,8 @@
 package com.example.motorvognregister;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 public class MotorvognController {
+
 }
