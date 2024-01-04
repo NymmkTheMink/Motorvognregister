f02433f11ecfb878cfccecf2c0d6389e906b116a
NymmkTheMink <141179443+NymmkTheMink@users.noreply.github.com>
2024-01-04 22:19:03 +0100

    finished motorvognController

--- src/main/java/com/example/motorvognregister/MotorvognController.java
+++ src/main/java/com/example/motorvognregister/MotorvognController.java
@@ -8,5 +8,17 @@ import java.util.List;
 
 @RestController
 public class MotorvognController {
+    public final List<Motorvogn> alleBiler = new ArrayList<>();
 
+    @GetMapping("/lagre")
+    public void lagreMotorvogn(Motorvogn innMotorvogn) { alleBiler.add(innMotorvogn);}
+    @GetMapping("/hentAlle")
+    public List<Motorvogn> hentAlle(){
+        return alleBiler;
+    }
+
+    @GetMapping("/slettAlle")
+    public void slettAlle(){
+        alleBiler.clear();
+    }
 }
