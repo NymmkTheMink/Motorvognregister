a4378cbcc0058b70190089b6d55594a99770e987
NymmkTheMink <141179443+NymmkTheMink@users.noreply.github.com>
2024-01-04 15:34:45 +0100

    imported classes from java.util package

--- src/main/java/com/example/motorvognregister/MotorvognController.java
+++ src/main/java/com/example/motorvognregister/MotorvognController.java
@@ -3,6 +3,10 @@ package com.example.motorvognregister;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class MotorvognController {
 
 }
