577d4166ed74d728c67db739ddd17ad3b659f2ef
NymmkTheMink <141179443+NymmkTheMink@users.noreply.github.com>
2024-01-04 22:38:05 +0100

    made a function to register cars

--- src/main/resources/static/motorvogn.js
+++ src/main/resources/static/motorvogn.js
@@ -1,5 +1,24 @@
 function regBil() {
+    const bil = {
+        persNum : $("#persNum").val(),
+        navn : $("#navn").val(),
+        adresse : $("#adresse").val(),
+        kjennetegn : $("#kjennetegn").val(),
+        bilmerke : $("#bilmerke").val(),
+        biltype : $("#biltype").val()
+    }
 
+    const url = "/lagre";
+    $.get(url, bil,function(retur){
+        hentAlle();
+    });
+
+    $("#persNum").val("");
+    $("#navn").val("");
+    $("#adresse").val("");
+    $("#kjennetegn").val("");
+    $("#bilmerke").val("");
+    $("#biltype").val("");
 
 }
 
